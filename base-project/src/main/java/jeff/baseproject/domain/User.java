efd1bfaf7765435bebe6c5789d62a06ed88ae567
서성빈 <souladin@naver.com>
2022-07-05 23:19:27 +0900

    password 임시 구현 snake to camel

--- base-project/src/main/java/jeff/baseproject/domain/User.java
+++ base-project/src/main/java/jeff/baseproject/domain/User.java
@@ -1,7 +1,10 @@
 package jeff.baseproject.domain;
 
 
+
 import java.security.InvalidParameterException;
+import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,14 +17,14 @@ public class User {
     private String password;
     private String email;
     private int permission;
-    private boolean is_staff;
-    private boolean is_active;
-    private Date create_datetime;
-    private Date last_login_datetime;
-    private Date refresh_password;
-    private int invalid_login_count;
+    private boolean isStaff;
+    private boolean isActive;
+    private Date createDatetime;
+    private Date lastLoginDatetime;
+    private Date refreshPassword;
+    private int invalidLoginCount;
 
-    public User(String name, String email, int permission, boolean is_staff){
+    public User(String name, String email, int permission, boolean isStaff){
         // 이메일 검증
         if(!isValidEmail(email))
         {
@@ -29,28 +32,17 @@ public class User {
         }
 
         this.name = name;
-        this.password = defaultPassword;
+
         this.email = email;
         this.permission = permission;
-        this.is_staff = is_staff;
-        this.is_active = true;
+        this.isStaff = isStaff;
+        this.isActive = true;
 
         Date now = new Date();
-        this.create_datetime = now;
-        this.last_login_datetime = now;
-        this.refresh_password = now;
-        this.invalid_login_count = 0;
-    }
-
-    public static boolean isValidEmail(String email) {
-        boolean err = false;
-        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(email);
-        if (m.matches()) {
-            err = true;
-        }
-        return err;
+        this.createDatetime = now;
+        this.lastLoginDatetime = now;
+        this.invalidLoginCount = 0;
+        changePassword(defaultPassword);
     }
 
     public Long getId() {
@@ -85,53 +77,6 @@ public class User {
         this.permission = permission;
     }
 
-    public boolean isIs_staff() {
-        return is_staff;
-    }
-
-    public void setIs_staff(boolean is_staff) {
-        this.is_staff = is_staff;
-    }
-
-    public boolean isIs_active() {
-        return is_active;
-    }
-
-    public void setIs_active(boolean is_active) {
-        this.is_active = is_active;
-    }
-
-    public Date getCreate_datetime() {
-        return create_datetime;
-    }
-
-    public void setCreate_datetime(Date create_datetime) {
-        this.create_datetime = create_datetime;
-    }
-
-    public Date getLast_login_datetime() {
-        return last_login_datetime;
-    }
-
-    public void setLast_login_datetime(Date last_login_datetime) {
-        this.last_login_datetime = last_login_datetime;
-    }
-
-    public Date getRefresh_password() {
-        return refresh_password;
-    }
-
-    public void setRefresh_password(Date refresh_password) {
-        this.refresh_password = refresh_password;
-    }
-
-    public int getInvalid_login_count() {
-        return invalid_login_count;
-    }
-
-    public void setInvalid_login_count(int invalid_login_count) {
-        this.invalid_login_count = invalid_login_count;
-    }
 
     public String getPassword() {
         return password;
@@ -139,6 +84,165 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public boolean isStaff() {
+        return isStaff;
+    }
+
+    public void setStaff(boolean staff) {
+        isStaff = staff;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Date getCreateDatetime() {
+        return createDatetime;
+    }
+
+    public void setCreateDatetime(Date createDatetime) {
+        this.createDatetime = createDatetime;
+    }
+
+    public Date getLastLoginDatetime() {
+        return lastLoginDatetime;
+    }
+
+    public void setLastLoginDatetime(Date lastLoginDatetime) {
+        this.lastLoginDatetime = lastLoginDatetime;
+    }
+
+    public Date getRefreshPassword() {
+        return refreshPassword;
+    }
+
+    public void setRefreshPassword(Date refreshPassword) {
+        this.refreshPassword = refreshPassword;
+    }
+
+    public int getInvalidLoginCount() {
+        return invalidLoginCount;
+    }
+
+    public void setInvalidLoginCount(int invalidLoginCount) {
+        this.invalidLoginCount = invalidLoginCount;
+    }
+
+    public static boolean isValidEmail(String email) {
+        boolean err = false;
+        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        if (m.matches()) {
+            err = true;
+        }
+        return err;
+    }
+
+    /**
+     * 비밀번호 검증 메소드
+     *
+     * @param password 비밀번호 문자열
+     * @return 오류 메시지
+     */
+    public static String isValidPassword(String password) {
+        // 최소 8자, 최대 20자 상수 선언
+        final int MIN = 8;
+        final int MAX = 20;
+
+        // 영어, 숫자, 특수문자 포함한 MIN to MAX 글자 정규식
+        final String REGEX =
+                "^((?=.*\\d)(?=.*[a-zA-Z])(?=.*[\\W]).{" + MIN + "," + MAX + "})$";
+        // 3자리 연속 문자 정규식
+        final String SAMEPT = "(\\w)\\1\\1";
+        // 공백 문자 정규식
+        final String BLANKPT = "(\\s)";
+
+        // 정규식 검사객체
+        Matcher matcher;
+
+        // 공백 체크
+        if (password == null || "".equals(password)) {
+            return "Detected: No Password";
+        }
+
+        // ASCII 문자 비교를 위한 UpperCase
+        String tmpPw = password.toUpperCase();
+        // 문자열 길이
+        int strLen = tmpPw.length();
+
+        // 글자 길이 체크
+        if (strLen > 20 || strLen < 8) {
+            return "Detected: Incorrect Length(Length: " + strLen + ")";
+        }
+
+        // 공백 체크
+        matcher = Pattern.compile(BLANKPT).matcher(tmpPw);
+        if (matcher.find()) {
+            return "Detected: Blank";
+        }
+
+        // 비밀번호 정규식 체크
+        matcher = Pattern.compile(REGEX).matcher(tmpPw);
+        if (!matcher.find()) {
+            return "Detected: Wrong Regex";
+        }
+
+        // 동일한 문자 3개 이상 체크
+        matcher = Pattern.compile(SAMEPT).matcher(tmpPw);
+        if (matcher.find()) {
+            return "Detected: Same Word";
+        }
+
+        // 연속된 문자 / 숫자 3개 이상 체크
+
+        // ASCII Char를 담을 배열 선언
+        int[] tmpArray = new int[strLen];
+
+        // Make Array
+        for (int i = 0; i < strLen; i++) {
+            tmpArray[i] = tmpPw.charAt(i);
+        }
+
+        // Validation Array
+        for (int i = 0; i < strLen - 2; i++) {
+            // 첫 글자 A-Z / 0-9
+            if ((tmpArray[i] > 47
+                    && tmpArray[i + 2] < 58)
+                    || (tmpArray[i] > 64
+                    && tmpArray[i + 2] < 91)) {
+                // 배열의 연속된 수 검사
+                // 3번째 글자 - 2번째 글자 = 1, 3번째 글자 - 1번째 글자 = 2
+                if (Math.abs(tmpArray[i + 2] - tmpArray[i + 1]) == 1
+                        && Math.abs(tmpArray[i + 2] - tmpArray[i]) == 2) {
+                    char c1 = (char) tmpArray[i];
+                    char c2 = (char) tmpArray[i + 1];
+                    char c3 = (char) tmpArray[i + 2];
+                    return "Detected: Continuous Pattern: \"" + c1 + c2 + c3 + "\"";
+                }
+            }
+        }
+        // Validation Complete
+        return ">>> All Pass";
+
+    }
+
+    public String changePassword(String password)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+
+        cal.add(Calendar.MONTH, 6);
+        this.refreshPassword = cal.getTime();
+        // TODO(Jeff) - 암호화 준비
+        String newPassword = defaultPassword;
+        return newPassword;
     }
 }
 
