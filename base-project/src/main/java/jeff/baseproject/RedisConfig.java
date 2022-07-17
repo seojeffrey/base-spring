673c2c621c98b43bdcecae150d38e33bc4d504ec
서성빈 <souladin@naver.com>
2022-07-18 08:49:38 +0900

    Redis pipeline, Default Serializer 치환

--- base-project/src/main/java/jeff/baseproject/RedisConfig.java
+++ base-project/src/main/java/jeff/baseproject/RedisConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import javax.persistence.EntityManager;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 @Configuration
 public class RedisConfig {
@@ -25,8 +28,28 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setDefaultSerializer(new UsAsciiStringSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
 
+    //\xaa 문자 등 안들어가게 DefaultSerializer 를 치환
+    class UsAsciiStringSerializer extends StringRedisSerializer {
+        String charset_STR = "US-ASCII";
+
+        @Override
+        public byte[] serialize(String string)
+        {
+            try{
+                return (string == null ? null : string.getBytes(charset_STR));
+            } catch (UnsupportedEncodingException e) {
+                return null;
+            }
+        }
+
+        @Override
+        public String deserialize(byte[] bytes){
+            return (bytes == null ? null : new String(bytes, Charset.forName(charset_STR)));
+        }
+    }
 }
--- base-project/src/test/java/jeff/baseproject/RedisTemplateTest.java
+++ base-project/src/test/java/jeff/baseproject/RedisTemplateTest.java
@@ -1,15 +1,14 @@
 package jeff.baseproject;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.*;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,12 +17,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RedisTemplateTest {
 
     @Autowired
-    private RedisTemplate<String, String> redisTemplate;
+    private RedisTemplate<Object, String> redisTemplate;
+
+    @AfterEach
+    public void afterEach()
+    {
+        // 아니 왜 flush 가 없지..?
+        Set<Object> keys = redisTemplate.keys("*");
+
+        /*
+        for (Object key: keys.toArray()) {
+            redisTemplate.delete(key);
+        }*/
+        //redisTemplate.delete(Objects.requireNonNull(keys).toArray());
+    }
 
     @Test
     void testStrings() {
         // given
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        ValueOperations<Object, String> valueOperations = redisTemplate.opsForValue();
         String key = "stringKey";
 
         // when
@@ -38,7 +50,7 @@ public class RedisTemplateTest {
     @Test
     void testSet() {
         // given
-        SetOperations<String, String> setOperations = redisTemplate.opsForSet();
+        SetOperations<Object, String> setOperations = redisTemplate.opsForSet();
         String key = "setKey";
 
         // when
@@ -55,7 +67,7 @@ public class RedisTemplateTest {
     @Test
     void testHash() {
         // given
-        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+        HashOperations<Object, Object, Object> hashOperations = redisTemplate.opsForHash();
         String key = "hashKey";
 
         // when
@@ -71,5 +83,54 @@ public class RedisTemplateTest {
 
         Long size = hashOperations.size(key);
         assertThat(size).isEqualTo(entries.size());
+    }
+
+    @Test
+    void testSortedSet() {
+        // given
+        ZSetOperations<Object, String> zSetOperations = redisTemplate.opsForZSet();
+        String key = "zSetKey";
+
+        // when
+        zSetOperations.add(key, "Hello", 2);
+        zSetOperations.add(key, "World", 1);
+
+        // then
+        Set<String> value = zSetOperations.reverseRange(key, 0, -1);
+
+        Set<String> expect = new HashSet<String>(){{
+            add("Hello");
+            add("World");
+        }};
+        assertThat(value).isEqualTo(expect);
+        // 동점이 있을 경우를 대비해 같은 score entries 를 가져옴(동점=같은 랭킹 일 경우)
+        Set<String> entries = zSetOperations.reverseRangeByScore(key, 1, 1);
+        // score 를 이용해 가장 높은 친구를 가져옴
+        long rank = zSetOperations.reverseRank(key, Objects.requireNonNull(entries).toArray()[0]);
+        assertThat(rank).isEqualTo(1);
+
+        Long size = zSetOperations.size(key);
+        assertThat(size).isEqualTo(expect.size());
+    }
+
+    @Test
+    void testTransactionSet() {
+        // given
+        String key = "zScoreKey";
+        redisTemplate.execute(new SessionCallback<Object>() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+
+                operations.watch(key);  // key값 변경 체크
+                operations.multi(); // 트랜잭션 Start
+                for(int i=0; i<10000; i++) {
+                    operations.opsForValue().increment(key, 1);
+                }
+                operations.exec(); // 트랜잭션 Commit
+                return null;
+            }
+        });
+        String value = redisTemplate.opsForValue().get(key);
+        assertThat(value).isEqualTo(Integer.toString(10000));
     }
 }
